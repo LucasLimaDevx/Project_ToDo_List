07b74502079eda0fc80b443881a27711dbfba88e
Lucas Lima <lucaslimadevx@gmail.com>
2025-11-27 17:49:41 -0300

    Create exception package and GlobalHandlerExecption class

---
+++ src/main/java/com/lucasdevx/todo_list/exception/handler/GlobalHandlerException.java
@@ -0,0 +1,8 @@
+package com.lucasdevx.todo_list.exception.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalHandlerException {
+
+}
